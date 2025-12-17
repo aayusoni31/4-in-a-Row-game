c0bf6a0ed750242fe5f23de0259790229b1fb5a9
Aayushi Verma <137631538+aayusoni31@users.noreply.github.com>
2025-12-17 16:19:23 +0530

    chore: remove ignored files from tracking

--- backend/utils/constants.js
+++
@@ -1,7 +0,0 @@
-export const ROWS = 6;
-export const COLS = 7;
-export const EMPTY = 0;
-export const PLAYER_1 = 1;
-export const PLAYER_2 = 2; // Can be a human or the Bot
-export const BOT_WAIT_TIME = 10000; // 10 seconds for matchmaking
-export const RECONNECT_WINDOW = 30000; // 30 seconds to rejoin
