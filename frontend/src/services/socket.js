ab3d90db8eadd4116a16b56cbfec196810ce3944
Aayushi Verma <137631538+aayusoni31@users.noreply.github.com>
2025-12-19 16:06:07 +0530

    feat: Refactor App component and add Docker Compose for Kafka and Zookeeper

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,6 +1,7 @@
+// Connecting Node.js backend imp step
+// here the websocket connection is established
 import { io } from "socket.io-client";
 
-// Connect to your Node.js backend
 const socket = io("http://localhost:3001");
 
 export default socket;
